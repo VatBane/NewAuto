d7e08059fbc841c7f54d0e52743a582556b38ebd
VatBane <70464844+VatBane@users.noreply.github.com>
2021-12-30 09:01:53 +0200

    Add files via upload

---
+++ Shop/Migrations/20211227235642_complete.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Migrations
+{
+    public partial class complete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    categoryName = table.Column<string>(nullable: true),
+                    desc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(nullable: false),
+                    lastName = table.Column<string>(nullable: false),
+                    adress = table.Column<string>(maxLength: 20, nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    phone = table.Column<string>(nullable: false),
+                    orderTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    shortDesc = table.Column<string>(nullable: true),
+                    longDesc = table.Column<string>(nullable: true),
+                    img = table.Column<string>(nullable: true),
+                    price = table.Column<int>(nullable: false),
+                    isFavourite = table.Column<bool>(nullable: false),
+                    available = table.Column<bool>(nullable: false),
+                    categoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Car_Category_categoryId",
+                        column: x => x.categoryId,
+                        principalTable: "Category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItem",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    carid = table.Column<int>(nullable: true),
+                    price = table.Column<int>(nullable: false),
+                    CartId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItem", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_CartItem_Car_carid",
+                        column: x => x.carid,
+                        principalTable: "Car",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    orderId = table.Column<int>(nullable: false),
+                    CarId = table.Column<int>(nullable: false),
+                    price = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Car_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Car",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Order_orderId",
+                        column: x => x.orderId,
+                        principalTable: "Order",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Car_categoryId",
+                table: "Car",
+                column: "categoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItem_carid",
+                table: "CartItem",
+                column: "carid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_CarId",
+                table: "OrderDetail",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_orderId",
+                table: "OrderDetail",
+                column: "orderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItem");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
